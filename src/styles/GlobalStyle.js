f8d81a5d883bc12b7cba37b7841ee3b14100642c
LuigiClaudio <gens.claudia@gmail.com>
2019-03-26 00:47:45 +0100

    WIP initial setup

---
+++ src/styles/GlobalStyle.js
@@ -0,0 +1,11 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+ body { 
+    color: rgba(0, 0, 0, 0.4);
+    font-family: sans-serif;  
+    font-size: 1rem; 
+  }
+`;
+
+export default GlobalStyle;
